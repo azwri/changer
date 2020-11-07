2dc3dac60c851ac2ab4bb1f68d4b9bf92e25c9ae
azwri <aazwri@gmail.com>
2020-11-08 01:14:46 +0300

    fix errors

--- changer/__init__.py
+++ changer/__init__.py
@@ -1,11 +1,11 @@
-from allnumbers import Numbers as __num__
+from .allnumbers import Numbers as __num__ 
 
 
-languages = ['english', 'arabic', 'hindi', 'persian', 'bengali',
+__languages = ['english', 'arabic', 'hindi', 'persian', 'bengali',
              'chinese_simple', 'chinese_complex', 'malayalam', 'thai', 'urdu']
 
-for language_1 in languages:
-    for language_2 in languages:
-        if language_1 != language_2:
-            locals()['{}_to_{}'.format(language_1, language_2)] = eval(
-                '__num__().{}_to_{}'.format(language_1, language_2))
+for __language_1 in __languages:
+    for __language_2 in __languages:
+        if __language_1 != __language_2:
+            locals()['{}_to_{}'.format(__language_1, __language_2)] = eval(
+                '__num__().{}_to_{}'.format(__language_1, __language_2))
